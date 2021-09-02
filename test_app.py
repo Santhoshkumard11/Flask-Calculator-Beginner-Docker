ad0cee4963cc17eb20a33945ad13447a3c4c9a40
SANTHOSH KUMAR D <santhoshkdhana@gmail.com>
2021-09-02 22:29:47 +0530

    Create test_app.py
    Added unit tests for check_posted_data function

---
+++ test_app.py
@@ -0,0 +1,36 @@
+import unittest
+
+from snippet import check_posted_data
+
+class TestSnippet(unittest.TestCase):
+    
+    def test_check_posted_data_first(self):
+        print(f"Testing function - {check_posted_data.__name__}")
+        
+        data_list = [[{"x": 184,"y": 323},"add",200], [{"x": 143,"y": 0},"division",302]
+                     , [{"x": 143},"division",301]]
+        
+        for data in data_list:
+            
+            result = check_posted_data(data[0],data[1])
+            
+            self.assertEqual(result, data[2])
+
+
+    def test_check_posted_data_second(self):
+        
+        data_list = [[{"x": 184, "y": 323}, "add", 200], [
+            {"x": 143, "y": 0}, "division", 302], [{"x": 143}, "division", 301]]
+
+        for data in data_list:
+
+            result = check_posted_data(data[0], data[1])
+
+            self.assertEqual(result, data[2])
+            
+            print(f"Test data {data}")
+
+
+if __name__ == "__main__":
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test_reports'))
